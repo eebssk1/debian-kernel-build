c9c77a1b933e5982b410695e00973df0e5f1ce26
EBK21 <chkd13303@gmail.com>
2021-12-06 19:16:10 +0800

    add build script *_*

---
+++ b.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+wget $(source)
+
+dpkg-deb --extract *.deb .
+tar -xf usr/src/linux-source-*.*
+
+rm -rf *.deb usr
+
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+sudo ./llvm.sh 12
+
+sudo apt install libelf-dev libssl-dev dwarves bc
+
+cd $(find -type d -name linux-source-*)
+
+cp ../config .config
+
+for a in ../*.patch
+do
+patch -i $a -s -f
+done
+
+make bindeb-pkg -j CC=clang-12 LD=ld.lld AS=llvm-as-12 NM=llvm-nm-12 AR=llvm-ar-12
