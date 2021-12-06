bf383d4109d9fff8d92969c413665fbbb6f83452
EBK21 <chkd13303@gmail.com>
2021-12-06 20:03:32 +0800

    awoo >_<

--- b.sh
+++ b.sh
@@ -11,7 +11,7 @@ wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
 sudo ./llvm.sh 12
 
-sudo apt install libelf-dev libssl-dev dwarves bc
+sudo apt install libelf-dev libssl-dev dwarves bc lld
 
 cd $(find -type d -name linux-source-*)
 
@@ -19,7 +19,7 @@ cp ../config .config
 
 for a in ../*.patch
 do
-patch -i $a -s -f
+patch -i $a -s -f -p1
 done
 
 make bindeb-pkg -j CC=clang-12 LD=ld.lld AS=llvm-as-12 NM=llvm-nm-12 AR=llvm-ar-12
