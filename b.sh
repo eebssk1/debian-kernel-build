684a08665f3d48f0ae369e43635e1f99cd969bf7
EBK21 <chkd13303@gmail.com>
2023-10-03 20:41:02 +0800

    update

--- b.sh
+++ b.sh
@@ -15,7 +15,7 @@ for a in ../*.patch; do
 patch -f -p1 -i $a
 done
 
-KCFLAGS="-Wno-ignored-optimization-argument -mllvm --polly=true -mllvm --polly-run-inliner=true -mllvm --polly-opt-max-constant-term=31 -mllvm --polly-opt-max-coefficient=31 -mllvm --polly-register-tiling=true -mllvm --polly-run-dce=true -mllvm --slp-min-reg-size=64 -mllvm --enable-partial-inlining=true -mllvm --extra-vectorizer-passes=true -mllvm --enable-loop-flatten=true -mllvm --hot-cold-split=true -mllvm --enable-gvn-hoist=true -mllvm --enable-matrix=true -mllvm --enable-constraint-elimination=true -mllvm --reroll-loops=true"
+KCFLAGS="-mllvm --polly=true -mllvm --polly-run-inliner=true -mllvm --polly-opt-max-constant-term=30 -mllvm --polly-opt-max-coefficient=30 -mllvm --polly-register-tiling=true -mllvm --polly-run-dce=true -mllvm --slp-min-reg-size=64 -mllvm --enable-partial-inlining=true -mllvm --extra-vectorizer-passes=true -mllvm --enable-loop-flatten=true -mllvm --enable-gvn-hoist=true -mllvm --enable-matrix=true -mllvm --enable-constraint-elimination=true -mllvm --reroll-loops=true"
 
 if [ "$1" = "desktop" ]; then
 export KCFLAGS="$KCFLAGS -mtune=skylake"
@@ -34,10 +34,10 @@ echo "No such target!"
 exit 1
 fi
 
-if [ "x$(which ccache)" != "x" ]; then
-export CC="ccache clang"
-fi
-
 make oldconfig LLVM=1  || exit 1
 
+if [ "x$(which ccache)" != "x" ]; then
+make bindeb-pkg -j2 LLVM=1 CC="ccache clang"
+else
 make bindeb-pkg -j2 LLVM=1
+fi
