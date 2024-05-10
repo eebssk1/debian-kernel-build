a418d3d12655fa8eea0272a1147377167a9e5795
EBK21 <chkd13303@gmail.com>
2024-05-10 16:26:33 +0800

    add gcc build

--- b.sh
+++ b.sh
@@ -11,10 +11,16 @@ apt-get install -y libelf-dev libssl-dev dwarves bc kmod cpio python3 zstd debhe
 
 curl -L https://gitlab.com/xanmod/linux/-/archive/$(cat version)-xanmod1.tar.bz2 | tar --bzip2 -xf - || exit 1
 
-curl -L https://github.com/eebssk1/aio_tc_build/releases/download/20240425_llvm/llvm_18.1.4.tgz | tar -zxf -
 
+if [ x$2 = xgcc ]; then
+curl -L https://github.com/eebssk1/aio_tc_build/releases/download/604ce955/x86_64-linux-gnu-native.tb2 | tar --bz2 -xf -
+mv x86_64-linux-gnu /opt/newcc
+chown -R root:root /opt/newcc
+else
+curl -L https://github.com/eebssk1/aio_tc_build/releases/download/20240425_llvm/llvm_18.1.4.tgz | tar -zxf -
 mv llvm_18.1.4 /opt/newclang
 chown -R root:root /opt/newclang
+fi
 
 echo VER=$(cat version) >> $GITHUB_ENV
 
@@ -47,8 +53,11 @@ if [ -e /opt/newclang/bin ]; then
 export PATH=/opt/newclang/bin:$PATH
 fi
 
-
+if [ x$2 = xgcc ]; then
+KCFLAGS="-O3 -fgraphite -fgraphite-identity -fipa-pta -flimit-function-alignment -flive-range-shrinkage -fsched-pressure -fsched-spec-load -fsched-stalled-insns=5 -fsched-stalled-insns-dep=8 -fschedule-insns -ftree-cselim -ftree-lrs -malign-data=cacheline -mrelax-cmpxchg-loop @$PWD/../gt.txt"
+else
 KCFLAGS="-O3 -mllvm --enable-knowledge-retention=true -mllvm --polly=true -mllvm --polly-vectorizer=stripmine -mllvm --polly-default-tile-size=32 -mllvm --polly-2nd-level-default-tile-size=8  -mllvm --polly-2nd-level-tiling=true -mllvm --polly-run-inliner=true -mllvm --polly-opt-max-constant-term=48 -mllvm --polly-opt-max-coefficient=48  -mllvm --polly-register-tiling=true -mllvm --polly-run-dce=true -mllvm --polly-detect-profitability-min-per-loop-insts=52 -mllvm --polly-invariant-load-hoisting=true -mllvm --extra-vectorizer-passes=true -mllvm --enable-loop-flatten=true -mllvm --enable-gvn-hoist=true -mllvm --enable-matrix=true -mllvm --enable-constraint-elimination=true -mllvm --enable-module-inliner=true -mllvm --thinlto-synthesize-entry-counts=true"
+fi
 
 if [ "$1" = "desktop" ]; then
 export KCFLAGS="$KCFLAGS -mtune=skylake"
@@ -67,12 +76,18 @@ echo "No such target!"
 exit 1
 fi
 
-make olddefconfig LLVM=1
+if [ x$2 = xgcc ]; then
+export CC=gcc
+else
+export CC=clang
+export LLVM=1
+fi
 
 if [ "x$(which ccache)" != "x" ]; then
 ccache -o compression_level=3
 ccache -o sloppiness=locale
-make bindeb-pkg -j3 LLVM=1 CC="ccache clang"
-else
-make bindeb-pkg -j3 LLVM=1
+export CC="ccache $CC"
 fi
+
+make olddefconfig
+make bindeb-pkg -j3
