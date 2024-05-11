cc258487c696372f7c282a061dceb07690cc80a6
EBK21 <chkd13303@gmail.com>
2024-05-11 17:50:20 +0800

    exit if patch fail

--- b.sh
+++ b.sh
@@ -23,19 +23,19 @@ cd linux-* || exit 1
 for a in ../patch/cp/*.patch; do
 [ -f "$a" ] || continue
 echo Applying $a
-patch -f -p1 -i $a
+patch -f -p1 -i $a || exit 128
 done
 
 for a in ../patch/cl/*.patch; do
 [ -f "$a" ] || continue
 echo Applying $a
-patch -f -p1 -i $a
+patch -f -p1 -i $a || exit 128
 done
 
 for a in ../patch/rev/*.patch; do
 [ -f "$a" ] || continue
 echo Revesing $a
-patch -R -p1 -i $a
+patch -R -p1 -i $a || exit 128
 done
 
 
