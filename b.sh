1deb16bf6089644a6dbe3498b546d3248d76f796
EBK21 <chkd13303@gmail.com>
2024-05-21 02:25:52 +0000

    update compiler flags

--- b.sh
+++ b.sh
@@ -54,7 +54,7 @@ export PATH=/opt/newclang/bin:$PATH
 fi
 
 if [ x$2 = xgcc ]; then
-KCFLAGS="-O3 -fgraphite -fgraphite-identity -fipa-pta -flimit-function-alignment -fmodulo-sched -freschedule-modulo-scheduled-loops -flive-range-shrinkage -fsched-pressure -fsched-spec-load -fsched-stalled-insns=5 -fsched-stalled-insns-dep=8 -fschedule-insns -ftree-lrs -malign-data=cacheline -mrelax-cmpxchg-loop @$PWD/../gp.txt"
+KCFLAGS="-O3 -fgraphite -fgraphite-identity -fipa-pta -fmodulo-sched -freschedule-modulo-scheduled-loops -flive-range-shrinkage -floop-nest-optimize -fsched-pressure -fsched-spec-load -fsched-stalled-insns=5 -fsched-stalled-insns-dep=10 -fschedule-insns -ftree-lrs -malign-data=cacheline -mrelax-cmpxchg-loop @$PWD/../gp.txt"
 else
 KCFLAGS="-O3 -mllvm --enable-knowledge-retention=true -mllvm --polly=true -mllvm --polly-vectorizer=stripmine -mllvm --polly-default-tile-size=32 -mllvm --polly-2nd-level-default-tile-size=8  -mllvm --polly-2nd-level-tiling=true -mllvm --polly-run-inliner=true -mllvm --polly-opt-max-constant-term=48 -mllvm --polly-opt-max-coefficient=48  -mllvm --polly-register-tiling=true -mllvm --polly-run-dce=true -mllvm --polly-detect-profitability-min-per-loop-insts=52 -mllvm --polly-invariant-load-hoisting=true -mllvm --extra-vectorizer-passes=true -mllvm --enable-loop-flatten=true -mllvm --enable-gvn-hoist=true -mllvm --enable-matrix=true -mllvm --enable-constraint-elimination=true -mllvm --enable-module-inliner=true -mllvm --thinlto-synthesize-entry-counts=true"
 fi
