838c4bc66e7180043dd8ea3c9bbd2853c793851a
EBK21 <chkd13303@gmail.com>
2024-07-23 01:44:37 +0800

    Update tc version

--- b.sh
+++ b.sh
@@ -13,7 +13,7 @@ curl -L https://gitlab.com/xanmod/linux/-/archive/$(cat version)-xanmod1.tar.bz2
 
 
 if [ x$2 = xgcc ]; then
-curl -L https://github.com/eebssk1/aio_tc_build/releases/download/8c4d2a0f/x86_64-linux-gnu-native.tb2 | tar --bz -xf -
+curl -L https://github.com/eebssk1/aio_tc_build/releases/download/5688233d/x86_64-linux-gnu-native.tb2 | tar --bz -xf -
 mv x86_64-linux-gnu /opt/newcc
 chown -R root:root /opt/newcc
 else
