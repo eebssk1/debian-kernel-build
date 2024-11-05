772cc5fd9dcab60ddeb38ac0b1ad67a434d48017
EBK21 <chkd13303@gmail.com>
2024-11-05 13:00:10 +0000

    add bbr v3 tune

--- b.sh
+++ b.sh
@@ -47,16 +47,6 @@ echo Applying $a
 patch -f -p1 -i $a || exit 128
 done
 
-for a in ../patch/bbr/*.patch; do
-[ -f "$a" ] || continue
-echo Revesing $a
-patch -R -p1 -i $a || exit 128
-done
-
-if [ -e ../bbr.c ]; then
-mv ../bbr.c net/ipv4/tcp_bbr.c
-fi
-
 if [ -e /opt/newcc/bin ]; then
 export PATH=/opt/newcc/bin:$PATH
 fi
