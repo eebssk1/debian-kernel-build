f6e69fbb1cfcc5130bb092c10287604bcdd1f67a
EBK21 <chkd13303@gmail.com>
2025-02-03 15:03:05 +0000

    disable gp.txt

--- b.sh
+++ b.sh
@@ -66,7 +66,7 @@ fi
 hash -r
 
 if [ x$2 = xgcc ]; then
-KCFLAGS="-fipa-pta -D__1ENABLE_AGGRESSIVE_GRAPHITE9__ @$PWD/../gp.txt"
+KCFLAGS="-fipa-pta -D__1ENABLE_AGGRESSIVE_GRAPHITE9__"
 else
 KCFLAGS="-mllvm --enable-knowledge-retention=true -mllvm --polly=true -mllvm --polly-vectorizer=stripmine -mllvm --polly-default-tile-size=30 -mllvm --polly-2nd-level-default-tile-size=8  -mllvm --polly-2nd-level-tiling=true -mllvm --polly-run-inliner=true -mllvm --polly-opt-max-constant-term=45 -mllvm --polly-opt-max-coefficient=45  -mllvm --polly-register-tiling=true -mllvm --polly-run-dce=true -mllvm --polly-detect-profitability-min-per-loop-insts=56 -mllvm --polly-invariant-load-hoisting=true -mllvm --extra-vectorizer-passes=true -mllvm --enable-loop-flatten=true -mllvm --enable-gvn-hoist=true -mllvm --enable-matrix=true -mllvm --enable-constraint-elimination=true -mllvm --enable-module-inliner=true -mllvm --thinlto-synthesize-entry-counts=true"
 fi
