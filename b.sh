288f5feaa9cad7f2e1bcffaff38bcd0ab67a3086
EBK21 <chkd13303@gmail.com>
2026-03-27 08:47:01 +0000

    update to 6.18.20 probably

--- b.sh
+++ b.sh
@@ -7,9 +7,9 @@ fi
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y libelf-dev libssl-dev dwarves bc kmod cpio python3 zstd debhelper pahole|| exit 1
+apt-get install -y libelf-dev libssl-dev dwarves bc kmod cpio python3 zstd debhelper pahole libdw-dev  || exit 1
 
-FILE=6.12.74-xanmod$(cat build).tar.bz2
+FILE=$(cat version)-xanmod$(cat build).tar.bz2
 
 DLOK=false
 COUNT=0
@@ -42,11 +42,6 @@ chown -R root:root /opt/newclang
 fi
 
 echo VER=$(cat version) >> $GITHUB_ENV
-
-for a in $(cat update); do
-wget https://cdn.kernel.org/pub/linux/kernel/v6.x/incr/patch-$a.xz
-xz -d patch-$a.xz
-done
 
 cd linux-* || exit 1
 
